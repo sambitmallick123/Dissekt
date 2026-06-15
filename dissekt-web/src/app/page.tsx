6f0a847584ff384d66fb88c5146b114b330b3111
Sambit Mallick <40597796+sambitmallick123@users.noreply.github.com>
2026-06-15 06:26:48 +0200

    fix: Landing Page

--- dissekt-web/src/app/page.tsx
+++ dissekt-web/src/app/page.tsx
@@ -1,11 +1,5 @@
-'use client';
 import LandingPage from '@/components/LandingPage';
 
 export default function Home() {
-  return (
-    <LandingPage
-      onTryFree={() => window.location.href = '/analyze'}
-      onSignIn={() => window.location.href = '/invite'}
-    />
-  );
+  return <LandingPage />;
 }
